a7a64981df576b39283acdbd2e3645b8b374c3cd
hernanponcetta <hernanponcetta@gmail.com>
2021-07-12 13:53:34 +0200

    Pillow library corrected

--- scripts/gmg-run-bash.sh
+++ scripts/gmg-run-bash.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run -ti -v $(pwd):/home/fenics/shared docker.io/26091985/gmg-fenics-torch
+docker run -ti -v $(pwd):/home/fenics/shared docker.io/26091985/gmg-fenics-torch /bin/bash
